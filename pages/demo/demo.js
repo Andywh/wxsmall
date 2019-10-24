43b2a41040312de1842727de279834000edbb498
songliang <liang.song@ximalaya.com>
2019-10-24 17:24:31 +0800

    first

---
+++ pages/demo/demo.js
@@ -0,0 +1,41 @@
+// demo.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        data:Object
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        demo:[{
+            image: '../image.png',
+            title: '显瘦中长款系带风衣',
+            describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+            count: '888',
+            delCount: '666'
+        },{
+            image: '../image.png',
+            title: '显瘦中长款系带风衣',
+            describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+            count: '888',
+            delCount: '666'
+        },{
+            image: '../image.png',
+            title: '显瘦中长款系带风衣',
+            describe: '柔软顺滑、上身挺括显瘦，垂坠飘逸、不易皱好打理。',
+            count: '888',
+            delCount: '666'
+        }]
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
