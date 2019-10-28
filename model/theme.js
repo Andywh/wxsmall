db70eeb0f9fe969f3a6ae66f9ccd069f8f6dd084
songliang <liang.song@ximalaya.com>
2019-10-28 16:16:50 +0800

    add

--- model/theme.js
+++ model/theme.js
@@ -5,12 +5,38 @@
 import {Http} from "../utils/http";
 
 class Theme {
-    static async getHomeLocationA() {
-        return await Http.request({
+    static locationA = 't-1'
+    static locationE = 't-2'
+    static locationF = 't-3'
+    static locationH = 't-4'
+
+    themes = []
+
+    async getThemes() {
+        const names = `${Theme.locationA},${Theme.locationE},${Theme.locationF},${Theme.locationH}`
+        this.themes =  await Http.request({
             url: `theme/by/names`,
             data: {
-                names: 't-1'
+                names
             }
+        })
+    }
+
+    async getHomeLocationA() {
+        return this.themes.find(t => t.name === Theme.locationA) // 比 for 循环可读性好很多
+    }
+
+    async getHomeLocationE() {
+        return this.themes.find(t => t.name === Theme.locationE) // 比 for 循环可读性好很多
+    }
+
+    static getHomeLocationESpu() {
+        return Theme.getThemeSpuByName(Theme.locationE)
+    }
+
+    static getThemeSpuByName(name) {
+        return Http.request({
+            url: `theme/name/${name}/with_spu`
         })
     }
 }
